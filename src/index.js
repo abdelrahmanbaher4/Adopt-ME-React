a549f28d2e24e116f024f0b12740d813659ea758
abdelrahmanbaher4 <abdulrahman2017170222@cis.asu.edu.eg>
2022-05-29 23:14:46 +0200

    updated

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import React, { StrictMode, useState } from "react";
+import { render } from "react-dom"; //pulling something of react-dom "render" from react-dom , no run time cost
+import SearchParams from "./SearchParams";
+import Details from "./Details";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import ThemeContext from "./ThemeContext";
+const App = () => {
+  // not destructring it becasue we are going to pass it to my context
+  const theme = useState("darkblue");
+  return (
+    <StrictMode>
+      <ThemeContext.Provider value={theme}>
+        <BrowserRouter>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Routes>
+            <Route path="/details/:id" element={<Details />}></Route>
+            <Route path="/" element={<SearchParams />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </ThemeContext.Provider>
+    </StrictMode>
+  );
+};
+render(<App />, document.querySelector(".root"));
